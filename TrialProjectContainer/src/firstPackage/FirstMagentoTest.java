209c371c7dd53029740a457139f58ff7ce27abf0
Edwin Nwofor <edwin.nwofor@yahoo.com>
2016-04-28 23:01:10 +0100

    Today's automation code for initial Page Object design.

--- TrialProjectContainer/src/firstPackage/FirstMagentoTest.java
+++ TrialProjectContainer/src/firstPackage/FirstMagentoTest.java
@@ -1,5 +1,44 @@
 package firstPackage;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import pages.BasePage;
+import pages.HomePage;
+import pages.MyAccountPage;
+import pages.MyDashboardPage;
+
 public class FirstMagentoTest {
 
+	private BasePage basePage;
+	private HomePage homePage;
+	private MyAccountPage myAccountPage;
+	private MyDashboardPage myDashboardPage;
+	
+	@Before
+	public void setUp() {
+		WebDriver driver = new ChromeDriver();
+		this.basePage = new BasePage(driver);
+		this.homePage = new HomePage(driver);
+		this.myAccountPage = new MyAccountPage(driver);
+		this.myDashboardPage = new MyDashboardPage(driver);
+		basePage.setupTest();
+	}
+	
+	@Test
+	public void magentoLoginTest() {
+		homePage.goToMyAccount();
+		myAccountPage.loginWith("234dotus@gmail.com", "password123");
+		myDashboardPage.validateLogin("Hello, Donald Jonathan!");		
+	}
+	
+	
+	@After
+	public void tearDown() {
+		basePage.cleanUpTest();
+	}
+	
 }
